5996243c382b30f6eee2f095f4087f0aebe9f880
mkhi33 <aarodriguezv@unah.hn>
2022-01-04 16:29:40 -0600

    Cotizador de criptomonedas

---
+++ criptos-react/src/data.js
@@ -0,0 +1,15 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'MXN', nombre: 'Peso Mexicano'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'GBP', nombre: 'Libra Esterlina'},
+    { id: 'HNL', nombre: 'Lempira Hondureño'},
+
+];
+
+const API_KEY = `2693b1dc7b10fca6e314b648100727655436f2b7ba3be1c917c3315bd4699eca`
+
+export {
+    monedas,
+    API_KEY
+}
