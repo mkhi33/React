bc0b14213f7a1a327a1b38c29ba4ade567f35bd6
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-03-10 12:52:07 -0600

    Se implementa carrito de compras en GuitarLa

--- gitarla_next/pages/_app.js
+++ gitarla_next/pages/_app.js
@@ -1,8 +1,54 @@
 import '../styles/normalize.css'
 import '../styles/globals.css'
+import { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [ carrito, setCarrito ] = useState([]);
+
+  useEffect( () => {
+    const carritoLs = JSON.parse(localStorage.getItem('carrito')) ?? [];
+    setCarrito(carritoLs);
+  }, [])
+  
+
+  useEffect( () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }, [carrito])
+
+  const agregarCarrito = producto => {
+  
+    if(carrito.some( articulo => articulo.id === producto.id)){
+      const carritoActualizado = carrito.map( articulo => {
+        if(articulo.id === producto.id ){
+          articulo.cantidad = producto.cantidad
+        }
+        return articulo
+      })
+      setCarrito(carritoActualizado);
+    }else {
+      console.log("Nuevo producto")
+    }
+
+    setCarrito([...carrito, producto]);
+  }
+
+  const actualizarCantidad = producto => {
+    const carritoActualizado = carrito.map( articulo => {
+      if(articulo.id === producto.id ){
+        articulo.cantidad = producto.cantidad
+      }
+      return articulo
+    })
+    setCarrito(carritoActualizado)
+  }
+
+  const eliminarProducto = id => {
+    const carritoActualizado = carrito.filter( articulo => articulo.id !== id)
+    setCarrito(carritoActualizado)
+  }
+
+  return <Component {...pageProps} carrito={carrito} agregarCarrito={agregarCarrito} actualizarCantidad={actualizarCantidad} eliminarProducto={eliminarProducto} />
 }
 
 export default MyApp
