7eec94eebe080f15d8fbb199f0fd348be6d8b6ba
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-03-10 13:02:12 -0600

    Agregando variables de entorno

--- guitarla_strapiV2/config/database.js
+++ guitarla_strapiV2/config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.d0xpv.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'guitarla_strapiV2'),
-        username: env('DATABASE_USERNAME', 'mkhi'),
-        password: env('DATABASE_PASSWORD', 'admin'),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
