87298c3aa14f75028739054969be6daf794244bb
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-03-10 14:20:31 -0600

    Proyecto GuitarLa finalizado

--- gitarla_next/components/Layout.js
+++ gitarla_next/components/Layout.js
@@ -12,9 +12,6 @@ const Layout = ({children, pagina, guitarra}) => {
             <Head>
                 <title>GitarLA - {pagina}</title>
                 <meta name="description" content="Sitio Web de venta de gitarras" />
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet"/>
 
             </Head>
             <Header
