85faa146ed1b48150c924fb295eff394fe10e626
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-03-14 18:15:36 -0600

    Proyecto buscador de letras V2 usando context

---
+++ letrasmusica/src/components/Alerta.jsx
@@ -0,0 +1,8 @@
+
+const Alerta = ({children}) => {
+  return (
+    <div className="alerta">{children}</div>
+  )
+}
+
+export default Alerta
---
+++ letrasmusica/src/context/LetrasProvider.jsx
@@ -0,0 +1,47 @@
+import { useState, createContext } from "react";
+import axios from "axios";
+const LetrasContext = createContext();
+
+const LetrasProvider = ({children}) => {
+
+    const [ alerta, setAlerta ] = useState("");
+    const [ letra, setLetra ] = useState("");
+    const [ cargando, setCargando ] = useState(false);
+    
+    const busquedaLetra = async (busqueda) => {
+        setCargando(true);
+        try {
+            const { artista, cancion } = busqueda;
+            const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+            const { data } = await axios(url)
+            setLetra(data.lyrics)
+            setAlerta("");
+
+        } catch (error) {
+            console.log(error);
+            setAlerta("Canción no encontrada")
+        }
+        setCargando(false)
+    }   
+
+
+    return (
+        <LetrasContext.Provider
+            value={{
+                alerta,
+                setAlerta,
+                busquedaLetra,
+                letra,
+                cargando
+            }}
+        >
+            {children}
+        </LetrasContext.Provider>
+    )
+}
+
+export {
+    LetrasProvider
+}
+
+export default LetrasContext;
