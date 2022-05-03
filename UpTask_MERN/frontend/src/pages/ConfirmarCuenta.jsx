5b662e0f32d6fb72d06efb609c9bc8f6365e440d
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-05-03 13:01:29 -0600

    Se crea la página para Confirnar cuenta y cambiar contraseña

--- UpTask_MERN/frontend/src/pages/ConfirmarCuenta.jsx
+++ UpTask_MERN/frontend/src/pages/ConfirmarCuenta.jsx
@@ -1,7 +1,14 @@
 
 const ConfirmarCuenta = () => {
   return (
-    <div>ConfirmarCuenta</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+          Confirma Tu Cuenta y Comienza a Crear tus
+          <span className="text-slate-700"> proyectos</span>
+      </h1>
+
+
+    </>
   )
 }
 
--- UpTask_MERN/frontend/src/pages/NuevaPassword.jsx
+++ UpTask_MERN/frontend/src/pages/NuevaPassword.jsx
@@ -1,7 +1,31 @@
 
 const NuevaPassword = () => {
   return (
-    <div>NuevaPassword</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+          Restablece tu password y no pierdas acceso a tus
+          <span className="text-slate-700"> proyectos</span>
+      </h1>
+      <form className="my-5 bg-white shadow rounded-lg p-10">
+
+          <div className="my-5 ">
+              <label htmlFor="password" className="uppercase text-gray-600 block text-xl font-bold">Nuevo Password</label>
+              <input 
+                  id="password"
+                  type="password"
+                  placeholder="Escribe Tu Nuevo Password"
+                  className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              />
+          </div>
+
+          <input 
+              type="submit"
+              value="Guardar Nuevo Password"
+              className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors" 
+          />
+      </form>
+
+    </>
   )
 }
 
