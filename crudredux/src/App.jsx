ddfe6d2064e2e2f77ac4dbfce9ee2a49dff7fd2b
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-05-10 14:00:49 -0600

    Configurando Redux

---
+++ crudredux/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Productos from './components/Productos'
+import NuevoProducto from './components/NuevoProducto'
+import EditarProducto from './components/EditarProducto'
+
+// Redux
+import { Provider } from 'react-redux'
+import store from './store'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <Header />
+        <div className='container mt-5'>
+          <Routes >
+            <Route path='/' element={<Productos />} />
+            <Route path='/productos/nuevo' element={<NuevoProducto />}/>
+            <Route path='/productos/editar/:id' element={<EditarProducto />}/>
+          </Routes>
+        </div>
+      </Provider>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
