16bc0179680cf76f94eed5ab94c02484c849006b
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-11 16:15:36 -0600

    Moviendo los nav del header a un componente Navegación

--- guitarla-remix/app/components/Header.jsx
+++ guitarla-remix/app/components/Header.jsx
@@ -1,19 +1,15 @@
-import { Link, useLocation } from '@remix-run/react'
+import {  } from '@remix-run/react'
 import logo from '../../public/img/logo.svg'
+import Navegacion from './Navegacion'
 const Header = () => {
-    const location = useLocation()
+    
   return (
     <header className="header">
         <div className="contenedor barra">
             <div className="logo">
                 <img className="logo" src={logo} alt="Logotipo GuitarLA" />
             </div>
-            <nav className="navegacion">
-                <Link className={location.pathname === '/' ? 'active' : ''} to='/'>Inicio</Link>
-                <Link className={location.pathname === '/nosotros' ? 'active' : ''} to='/nosotros'>Nosotros</Link>
-                <Link className={location.pathname === '/tienda' ? 'active' : ''} to='/tienda'>Tienda</Link>
-                <Link className={location.pathname === '/blog' ? 'active' : ''} to='/blog'>Blog</Link>
-            </nav>
+            <Navegacion />
         </div>
     </header>
   )
