3138beea7a180c304a83983cc8770346bcd3a1b1
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-11 16:06:19 -0600

    Configurando archivo root

--- guitarla-remix/app/root.jsx
+++ guitarla-remix/app/root.jsx
@@ -1,31 +1,70 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+    Meta,
+    Links,
+    Outlet,
+    Scripts,
+    LiveReload
+} from '@remix-run/react'
 
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import styles from '~/styles/index.css'
+import Header from '~/components/Header'
+
+export function meta() {
+    return ({
+        charset: 'utf-8',
+        title: 'GuitarLA - Remix',
+        viewport: "width=device-width,initial-scale=1"
+    })
+}
+export function links() {
+    return ([
+        {
+            rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossOrigin: "true"
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;700&family=Outfit:wght@400;700;900&display=swap'
+        },
+        {
+            rel: 'stylesheet',
+            href: styles
+        }, 
+
+    ])
+}
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <Document>
+            <Outlet />
+        </Document>
+    )
+}
+
+
+function Document({children}) {
+    return (
+        <html lang="es">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Header />
+                {children}
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    )
 }
