a07736c99f7866afb1fb253c4a5836291824d198
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-11 21:10:26 -0600

    Mostrando la entrada del blog

--- guitarla-remix/app/routes/blog.jsx
+++ guitarla-remix/app/routes/blog.jsx
@@ -4,6 +4,14 @@ import { getPosts } from '~/models/posts.server'
 
 import styles from '~/styles/blog.css'
 
+
+export function meta(){
+  return {
+    title: `GuitarLA - Nuestro Blog`,
+    description: `GuitarLA, Blog de música y venta de guitarras`
+  }
+}
+
 export async function loader() {
   const posts = await getPosts();
   return posts.data;
---
+++ guitarla-remix/app/routes/posts/$postUrl.jsx
@@ -0,0 +1,56 @@
+import { useLoaderData } from "@remix-run/react"
+import { getPost } from "~/models/posts.server"
+import { formatearFecha } from "~/utils/helpers"
+import styles from '~/styles/blog.css'
+
+export function meta({data}) {
+    if(!data) {
+      return {
+        title: 'GuitarLA - Entrada no encontrada',
+        descripcion: `Guitarras, venta de guitarras, entrada no encontrada`
+      }
+    }
+  
+      return {
+          title: `GuitarLA - ${data.data[0].attributes.titulo}`,
+          desctiption: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.titulo}`
+      }
+  }
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+export async function loader({params}) {
+    const { postUrl } = params
+    const post = await getPost(postUrl)
+    if(post.data.length === 0) {
+        throw new Response('', {
+            status:404,
+            statusText: 'Entrada no encontrada'
+        })
+    }
+    return post
+}
+
+const Post = () => {
+
+    const post = useLoaderData()
+    const { titulo, contenido, imagen, publishedAt } = post?.data[0]?.attributes;
+  return (
+    <article className="contenedor post mt-3">
+        <img className="imagen" src={imagen?.data?.attributes?.url} alt={`Imagen del blog ${titulo}`} />
+        <div className="contenido">
+            <h3>{titulo}</h3>
+            <p className="fecha">{formatearFecha(publishedAt)}</p>
+            <p className="texto">{contenido}</p>
+        </div>
+    </article>
+  )
+}
+
+export default Post
