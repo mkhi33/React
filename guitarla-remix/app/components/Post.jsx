42d63008fe3f02d7ceeca77a1fee1297a237b859
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-11 20:43:57 -0600

    Mostrando la fecha en formato string

--- guitarla-remix/app/components/Post.jsx
+++ guitarla-remix/app/components/Post.jsx
@@ -1,5 +1,5 @@
 import { Link } from "@remix-run/react";
-
+import { formatearFecha } from "~/utils/helpers";
 
 const Post = ({post}) => {
     const { contenido, imagen, titulo, url, publishedAt } = post;
@@ -8,7 +8,7 @@ const Post = ({post}) => {
         <img className="imagen" src={imagen.data.attributes.formats.small.url} alt={`Imagen del blog ${titulo}`} />
         <div className="contenido">
             <h3>{titulo}</h3>
-            <p className="fecha">{publishedAt}</p>
+            <p className="fecha">{formatearFecha(publishedAt)}</p>
             <p className="resumen">{contenido}</p>
             <Link className="enlace" to={`/posts/${url}`} >Leer Post</Link>
         </div>
