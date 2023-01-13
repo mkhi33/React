adc1dd8f7a5228b78b2772ee9da75c3744e2859a
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-13 13:25:03 -0600

    Solucionando problema de hidratación

--- guitarla-remix/app/routes/carrito.jsx
+++ guitarla-remix/app/routes/carrito.jsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from '@remix-run/react'
 import { useEffect, useState } from 'react'
+import { ClientOnly } from 'remix-utils'
 import styles from '~/styles/carrito.css'
 
 export function links() {
@@ -23,47 +24,51 @@ const Carrito = () => {
         setTotal(calculoTotal)
     }, [carrito])
   return (
-    <main className="contenedor">
-        <h1 className="heading">Carrito de compras</h1>
-        <div className="contenido">
+    <ClientOnly fallback={'cargando...'}>
+        { () => (
+            <main className="contenedor">
+                <h1 className="heading">Carrito de compras</h1>
+                <div className="contenido">
 
-            <div className="carrito">
-                <h2>Articulos</h2>
-                {carrito?.length === 0 ? <p>No hay articulos en el carrito</p> : (
-                    carrito?.map( producto => (
-                        <div className='producto' key={producto.id} >
-                            <div>
-                                <img src={producto.imagen} alt={`Imagen del producto ${producto.nombre}`} />
-                            </div>
-                            <div>
-                                <p className='nombre'>{producto.nombre}</p>
-                                <p className='cantidad'>Cantidad:</p>
-                                <select onChange={ (e) => actualizarCantidad({
-                                    cantidad: parseInt(e.target.value),
-                                    id: producto.id
-                                })} className='select' value={producto.cantidad}>
-                                    <option value="">-- Seleccione --</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                                <p className='precio'>$<span>{producto.precio}</span></p>
-                                <p className='subtotal'>Subtotal: $<span>{ producto.cantidad * producto.precio}</span></p>
-                                <button onClick={() => eliminarGuitarra(producto.id)} type='button' className='btn-eliminar'>x</button>
-                            </div>
-                        </div>
-                    ))
-                )}
-            </div>
-            <aside className="resumen">
-                <h3>Resumen del pedido</h3>
-                <p>Total a pagar: ${total}</p>
-            </aside>
+                    <div className="carrito">
+                        <h2>Articulos</h2>
+                        {carrito?.length === 0 ? <p>No hay articulos en el carrito</p> : (
+                            carrito?.map( producto => (
+                                <div className='producto' key={producto.id} >
+                                    <div>
+                                        <img src={producto.imagen} alt={`Imagen del producto ${producto.nombre}`} />
+                                    </div>
+                                    <div>
+                                        <p className='nombre'>{producto.nombre}</p>
+                                        <p className='cantidad'>Cantidad:</p>
+                                        <select onChange={ (e) => actualizarCantidad({
+                                            cantidad: parseInt(e.target.value),
+                                            id: producto.id
+                                        })} className='select' value={producto.cantidad}>
+                                            <option value="">-- Seleccione --</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                        </select>
+                                        <p className='precio'>$<span>{producto.precio}</span></p>
+                                        <p className='subtotal'>Subtotal: $<span>{ producto.cantidad * producto.precio}</span></p>
+                                        <button onClick={() => eliminarGuitarra(producto.id)} type='button' className='btn-eliminar'>x</button>
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                    <aside className="resumen">
+                        <h3>Resumen del pedido</h3>
+                        <p>Total a pagar: ${total}</p>
+                    </aside>
 
-        </div>
-    </main>
+                </div>
+            </main>
+        )}
+    </ClientOnly>
   )
 }
 
