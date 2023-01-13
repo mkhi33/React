55948ec0813a9f90694683b4e9f395c2b08ed1cf
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-13 10:48:08 -0600

    Agregando link de carrito a la navegación

--- guitarla-remix/app/components/Navegacion.jsx
+++ guitarla-remix/app/components/Navegacion.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from '@remix-run/react'
-
+import imagen from '../../public/img/carrito.png'
 
 const Navegacion = () => {
     const location = useLocation()
@@ -9,6 +9,10 @@ const Navegacion = () => {
         <Link className={location.pathname === '/nosotros' ? 'active' : ''} to='/nosotros'>Nosotros</Link>
         <Link className={location.pathname === '/guitarras' ? 'active' : ''} to='/guitarras'>Tienda</Link>
         <Link className={location.pathname === '/blog' ? 'active' : ''} to='/blog'>Blog</Link>
+        <Link to='/carrito'>
+          <img src={imagen} alt="Imagen de carrito de compras" />
+        </Link>
+
     </nav>
   )
 }
