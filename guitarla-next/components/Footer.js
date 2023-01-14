a6603fd316ee270b6e5a31c9f86c3979394ed538
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-14 16:16:19 -0600

    Creando el footer

---
+++ guitarla-next/components/Footer.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import styles from '@/styles/Footer.module.css'
+const Footer = () => {
+
+  return (
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido}`}>
+        <nav className={styles.navegacion}>
+          <Link href='/'>Inicio</Link>
+          <Link href='/nosotros'>Nosotros</Link>
+          <Link href='/blog'>Blog</Link>
+          <Link href='/tienda'>Tienda</Link>
+        </nav>
+        <p className={styles.copyrigth}>© {new Date().getFullYear()} - Todos los derechos reservados</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
