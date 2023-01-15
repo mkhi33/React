d811591cf3cb0352e57a1b8e7ff94929dcc7a7c2
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-15 15:22:01 -0600

    importando logo

--- guitarla-next/components/Header.js
+++ guitarla-next/components/Header.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/Header.module.css'
 import { useRouter } from 'next/router'
+import logo from '@/public/img/logo.svg'
 const Header = () => {
 
   const router = useRouter()
@@ -9,7 +10,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={`contenedor ${styles.barra}`}>
-          <Image src='img/logo.svg' width={300} height={40} alt=""/>
+          <Image src={logo} width={300} height={40} alt=""/>
           <nav className={styles.navegacion}>
               <Link className={router.pathname === '/' ? styles.active : ''} href='/'>Inicio</Link>
               <Link className={router.pathname === '/nosotros' ? styles.active : ''} href='/nosotros'>Nosotros</Link>
