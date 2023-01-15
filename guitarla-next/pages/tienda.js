3ed4c1e11a582ad7566ef62b71063a55ffbf1815
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-15 12:32:51 -0600

    Listando guitarras en la tienda

--- guitarla-next/pages/tienda.js
+++ guitarla-next/pages/tienda.js
@@ -1,11 +1,42 @@
+import Guitarra from "@/components/Guitarra"
 import Layout from "@/components/Layout"
+import styles from '@/styles/Grid.module.css'
 
-const Tienda = () => {
+const Tienda = ({guitarras}) => {
+  console.log(guitarras)
   return (
     <Layout title={'Tienda'} description="Tienda virtual, venta de guitarras, instrumentos, GuitarLA">
-        <h1>Tienda</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra colección</h1>
+        <div className={styles.grid}>
+          {guitarras?.map( guitarra => ( 
+            <Guitarra 
+            key={guitarra.id} 
+            guitarra={guitarra.attributes} /> 
+            ))}
+        </div>
+      </main>
     </Layout>
   )
 }
 
 export default Tienda
+
+// export async function getStaticProps() {
+//   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+//   const { data:guitarras } = await respuesta.json()
+//   return {
+//     props: {
+//       guitarras
+//     }
+//   }
+// }
+export async function getServerSideProps() {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+  const { data:guitarras } = await respuesta.json()
+  return {
+    props: {
+      guitarras
+    }
+  }
+}
