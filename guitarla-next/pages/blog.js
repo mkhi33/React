95f54e57513ec287ac746f707f109ecf1c9399a3
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-15 15:57:10 -0600

    Listando blogs

--- guitarla-next/pages/blog.js
+++ guitarla-next/pages/blog.js
@@ -1,11 +1,26 @@
 import Layout from "@/components/Layout"
+import Post from "@/components/Post"
+import styles from '@/styles/Grid.module.css'
 
-const Blog = () => {
+const Blog = ({posts}) => {
   return (
     <Layout title={'Blog'} description="Blog de música, venta de guitarras, consejos, GuitarLA">
-        <h1>Blog</h1>
+      <main className="contenedor">
+        <h1 className="heading">Blog</h1>
+        <div className={styles.grid}>{posts?.map( post => ( <Post key={post.id} post={post.attributes} />) )}</div>
+      </main>
     </Layout>
   )
 }
 
 export default Blog
+
+export async function getStaticProps() {
+  const respuesta = await fetch(`${process.env.API_URL}/posts?populate=imagen`)
+  const { data:posts } = await respuesta.json()
+  return {
+    props: {
+      posts
+    }
+  }
+}
