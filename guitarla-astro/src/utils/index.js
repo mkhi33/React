09f8e9d49eae40e206c68ea0255a938540b02738
Amilcar Rodriguez <aarodriguezv@unah.hn>
2023-01-16 17:11:19 -0600

    Implementando helper para formatear fecha

---
+++ guitarla-astro/src/utils/index.js
@@ -0,0 +1,7 @@
+export const formatearFecha = (fecha) => {
+    const fechaNueva = new Date(fecha);
+    return fechaNueva.toLocaleDateString('es-ES', {
+        day: '2-digit',
+        year: 'numeric',
+        month: 'long'
+    })}
