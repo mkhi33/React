d1b5703356fc14c1acd6602a458f385923757033
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-01-29 16:32:31 -0600

    Agregando links en el header

--- nodesend-cliente/src/components/Header.js
+++ nodesend-cliente/src/components/Header.js
@@ -1,10 +1,17 @@
 import Image from "next/image"
+import Link from "next/link"
 
 
 const Header = () => {
   return (
     <div className="py-8 flex flex-col md:flex-row items-center justify-between">
-        <Image className="w-64 mb-8 md:mb-0" src='./logo.svg' alt="Logo Nodesend" width={100} height={15} />
+        <Link href="/">
+            <Image className="w-64 mb-8 md:mb-0" src='./logo.svg' alt="Logo Nodesend" width={100} height={15} />
+        </Link>
+        <div className="flex gap-3">
+            <Link className="bg-red-500 px-5 py-3 rounded text-white font-bold uppercase" href={'/login'}>Iniciar sesión</Link>
+            <Link className="bg-black px-5 py-3 rounded text-white font-bold uppercase" href={'/signin'}>Crear Cuenta</Link>
+        </div>
     </div>
   )
 }
