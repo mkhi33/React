6c25b89445628d441ad40029ee32657e1d5cc75f
Amilcar Rodriguez <aar.velasquez@gmail.com>
2023-01-31 12:16:11 -0600

    Usando props para componente de Alertas

--- nodesend-cliente/src/components/Alert.js
+++ nodesend-cliente/src/components/Alert.js
@@ -1,9 +1,8 @@
-import { useContext } from 'react'
-import authContext from '@/context/auth/authContext'
 
-const Alert = () => {
-    const AuthContext = useContext(authContext)
-    const { message, error } = AuthContext
+
+const Alert = ({ message, error}) => {
+
+
   return (
     <div className={`${error ? 'bg-red-500' : 'bg-green-400'} py-2 w-full my-3 max-w-lg text-center text-white font-bold mx-auto`}>
         {message}
